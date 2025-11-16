02f875b527c2ff738c47fad718214f1992dcab8b
hung123 <nguyencuingvlhl@gmail.com>
2025-11-16 22:59:40 +0700

    first commit

---
+++ src/main/java/com/doan/bepsachviet_be/BepsachvietBeApplication.java
@@ -0,0 +1,13 @@
+package com.doan.bepsachviet_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BepsachvietBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BepsachvietBeApplication.class, args);
+	}
+
+}
