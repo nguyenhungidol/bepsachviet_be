e48b02824d2f87444e928202abc22b77eabe943f
hung123 <nguyencuingvlhl@gmail.com>
2025-11-16 22:59:40 +0700

    first commit

---
+++ src/test/java/com/doan/bepsachviet_be/BepsachvietBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doan.bepsachviet_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BepsachvietBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
